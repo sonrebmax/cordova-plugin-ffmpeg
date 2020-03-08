e07162187073eeee3d8af30ffe81966384cefbd5
marebison <rayonsi@gmail.com>
2020-03-08 14:33:26 +0000

    Update FFMpeg.java

--- src/android/FFMpeg.java
+++ src/android/FFMpeg.java
@@ -33,7 +33,7 @@ if (rc == RETURN_CODE_SUCCESS) {
 } else {
    // Log.i(Config.TAG, String.format("Command execution failed with rc=%d and the output below.", rc));
     // message =  String.format("Command execution failed with rc=%d and the output below.", rc);
-        message =   Log.INFO; //  "Command execution failed" +
+        message =    Config.getLastCommandOutput(); //  "Command execution failed" +
   //  Config.printLastCommandOutput(Log.INFO);
 }
 
