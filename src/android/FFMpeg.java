3cf9c5ef8fb29217b07316d42d6c165a3b569a1d
marebison <rayonsi@gmail.com>
2020-03-08 14:02:21 +0000

    Update FFMpeg.java

--- src/android/FFMpeg.java
+++ src/android/FFMpeg.java
@@ -3,7 +3,9 @@ package com.marin.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import com.arthenica.mobileffmpeg.Config;
 import com.arthenica.mobileffmpeg.FFmpeg;
+
 
 public class FFMpeg extends CordovaPlugin {
 
@@ -15,8 +17,25 @@ public class FFMpeg extends CordovaPlugin {
             //TODO: call ffmpeg-mobile and do the processing ...
             //FFmpeg.execute("-i file1.mp4 -c:v mpeg4 file2.mp4"); 
             //https://github.com/tanersener/mobile-ffmpeg/wiki/Android
-            String name = data.getString(0);
-            String message = "Hello, " + name;
+            String command_name = data.getString(0);
+            String message = "Command... " + command_name;
+
+
+int rc = FFmpeg.execute(command_name); // "-i file1.mp4 -c:v mpeg4 file2.mp4");
+
+if (rc == RETURN_CODE_SUCCESS) {
+//    Log.i(Config.TAG, "Command execution completed successfully.");
+    message = "Command execution completed successfully.";
+} else if (rc == RETURN_CODE_CANCEL) {
+   // Log.i(Config.TAG, "Command execution cancelled by user.");
+     message =  "Command execution cancelled by user.";
+} else {
+   // Log.i(Config.TAG, String.format("Command execution failed with rc=%d and the output below.", rc));
+    message =  String.format("Command execution failed with rc=%d and the output below.", rc);
+  //  Config.printLastCommandOutput(Log.INFO);
+}
+
+
             callbackContext.success(message);
 
             return true;
@@ -28,3 +47,5 @@ public class FFMpeg extends CordovaPlugin {
         }
     }
 }
+
+ 
