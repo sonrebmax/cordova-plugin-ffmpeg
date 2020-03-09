c694a27da90cbb85b8682706a561de10ccc1eeb5
marebison <rayonsi@gmail.com>
2020-03-09 11:01:15 +0000

    Update ffmpeg.js

--- www/ffmpeg.js
+++ www/ffmpeg.js
@@ -1,7 +1,53 @@
-/*global cordova, module*/
+var exec = require('cordova/exec');
 
-module.exports = {
-    exec: function (cmd, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "FFMpeg", "exec", [cmd]);
-    }
+exports.exec = function(cmd, success, error) {
+    exec(success, error, "FFMpeg", "exec", [cmd]);
 };
+
+exports.ffprobe = function(cmd, success, error) {
+    exec(success, error, "FFMpeg", "ffprobe", [cmd]);
+};
+
+exports.getMediaInformation = function(filename, success, error) {
+    exec(success, error, "FFMpeg", "getMediaInformation", [filename]);
+};
+
+exports.cancel = function(success, error) {
+    exec(success, error, "FFMpeg", "cancel", []);
+};
+
+exports.getLastCommandOutput = function(success, error) {
+    exec(success, error, "FFMpeg", "getLastCommandOutput", []);
+};
+
+exports.getLastReturnCode = function(success, error) {
+    exec(success, error, "FFMpeg", "getLastReturnCode", []);
+};
+
+exports.getLastReceivedStatistics = function(success, error) {
+    exec(success, error, "FFMpeg", "getLastReceivedStatistics", []);
+};
+
+exports.getSupportedCameraIds = function(success, error) {
+    exec(success, error, "FFMpeg", "getSupportedCameraIds", []);
+};
+
+exports.getFFmpegVersion = function(success, error) {
+    exec(success, error, "FFMpeg", "getFFmpegVersion", []);
+};
+
+exports.getVersion = function(success, error) {
+    exec(success, error, "FFMpeg", "getVersion", []);
+};
+
+exports.getNativeFFmpegVersion = function(success, error) {
+    exec(success, error, "FFMpeg", "getNativeFFmpegVersion", []);
+};
+
+// /*global cordova, module*/
+
+// module.exports = {
+//     exec: function (cmd, successCallback, errorCallback) {
+//         cordova.exec(successCallback, errorCallback, "FFMpeg", "exec", [cmd]);
+//     }
+// };
